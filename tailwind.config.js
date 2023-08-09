623322e084e1fec84375e0c36706b3386602cf65
Mostafijur-Rahman <mosta5617@gmail.com>
2023-08-09 23:47:40 +0600

    header logo search bar design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./*html"],
   theme: {
-    extend: {},
+    screens : {
+    sm : '576px',
+    md : '768px',
+    lg : '992px',
+    xl : '1200px',
+  },
+  container : {
+    center : true,
+    padding : '1rem'  // 16px
+  },
+    extend: {
+      colors : {
+        primary : '#FD3D57',
+      },
+      fontFamily:{
+        'borel' :['Borel'], 
+        'roboto' :['Roboto'], 
+        'openSans' :['Open+Sans'], 
+      }
+    },
+
   },
   plugins: [],
 }
