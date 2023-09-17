aa85a8b215495bf5047463889b98984b66934e75
Mostafijur-Rahman <mosta5617@gmail.com>
2023-09-17 16:23:26 +0600

    tailwind pintarest page setup

---
+++ datatype.js
@@ -0,0 +1,20 @@
+
+let age = '30';
+let nameP = "Name";
+let text =null;
+
+let Person={
+    firstName: "Mostafijur ",
+    lastName: " Rahman",
+    hobby: ["a","b","c","d"],
+    function(){
+        return this.firstName + this.lastName;
+    }
+}
+// console.log(Person.function(), Person.hobby[2]);
+
+let pi ="3.1416";
+
+
+console.log(typeof(pi), typeof(Number(pi)));
+
